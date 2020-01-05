ee991767d515598281e3677ca2a7a78e1761be9f
diegodisant <developerdiego0@gmail.com>
2020-01-05 11:05:56 -0600

    refactor: remove unused scripts

--- build-all.sh
+++ build-all.sh
@@ -29,11 +29,11 @@ echo
 
 echo "Building docker environment ....."
 rm serpent-cli
-docker stop bmex-go-serpent-aes
-docker rm bmex-serpent
-docker rmi bmex-go-serpent-aes
-docker build -t bmex-go-serpent-aes .
-docker run -it --name bmex-serpent bmex-go-serpent-aes:latest
+docker stop serpent-aes
+docker rm serpent-aes
+docker rmi serpent-aes
+docker build -t serpent-aes .
+docker run -it --name serpent-aes serpent-aes:latest
 
 echo
 echo "-----"
